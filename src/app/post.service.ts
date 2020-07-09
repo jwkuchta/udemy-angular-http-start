3fdff55858344063df1d0e044706f9562ddb20ae
Justina Kuchta <justyna.kuchta@gmail.com>
2020-07-09 11:00:08 -0400

    http events

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core'
-import { HttpClient, HttpHeaders } from '@angular/common/http'
-import { map, catchError } from 'rxjs/operators'
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http'
+import { map, catchError, tap } from 'rxjs/operators'
 import { Post } from './post.model';
 import { Subject, throwError } from 'rxjs';
 
@@ -17,13 +17,18 @@ export class PostService {
     createAndStorePosts(title: string, content: string) {
         const postData: Post = {title, content}
         this.http // http from line 15
-      // endpoint, body. Angular converts postData into JSON data
-      .post(postsUrl, postData)
-      // if you are not subscribing tot the http request, the request will not be sent
-      .subscribe(
-          responseData => console.log(responseData),
-          error => this.error.next(error)
-      )
+        .post(
+            postsUrl, 
+            postData,
+            {
+                observe: 'response' // this tells Angular to parse the full http response
+            }
+            )
+        // if you are not subscribing tot the http request, the request will not be sent
+        .subscribe(
+            responseData => console.log(responseData),
+            error => this.error.next(error)
+        )
     }
 
     fetchPosts() {
@@ -32,7 +37,9 @@ export class PostService {
         .get<{[key: string]: Post}>(
             postsUrl, 
             { 
-                headers: new HttpHeaders({ 'Custom-Headers': 'Hello' })
+                headers: new HttpHeaders({ 'Custom-Headers': 'Hello' }),
+                // Firebase allows for this query param, depends on API
+                params: new HttpParams().set('print', 'pretty')
             }) // we can define the type of the response in the get method or in the map func
         .pipe(map(responseData => {
             const postsArray: Post[] = []
@@ -49,6 +56,11 @@ export class PostService {
     }
 
     deletePosts() {
-        return this.http.delete(postsUrl)
+        return this.http.delete(
+            postsUrl,
+            {
+                observe: 'events'
+            }
+        ).pipe(tap(event => console.log(event)))
     }
 }
